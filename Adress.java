00046b92bb60c9cd4884c7ac47745f6fba212b10
JulioR2022 <129457084+JulioR2022@users.noreply.github.com>
2023-04-25 15:59:45 -0300

    Add files via upload

---
+++ Adress.java
@@ -0,0 +1,73 @@
+package interprises;
+public class Adress{
+    //Atributes
+    private String country;
+    private String state;
+    private String city;
+    private String street;
+    private int number;
+
+    //GETTERS AND SETTERS
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return  this.country;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setNumber(int number) {
+        if(number >= 0){
+            this.number = number;
+        }else {
+            this.number = 0;
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    // Construct
+
+    public Adress(String country, String state, String city, String street, int number){
+        this.setCountry(country);
+
+        this.setState(state);
+
+        this.setCity(city);
+
+        this.setStreet(street);
+
+        this.setNumber(number);
+    }
+
+    
+
+
+}
